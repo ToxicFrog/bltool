51bbdfa122bcffe1acdb4e6018f38cabe7badfe3
Ben Kelly <bk@ancilla.ca>
2013-11-08 19:12:43 -0500

    Add steam2backloggery.bat and basic release script

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VERSION="$1"
+DIR="bltool-$VERSION"
+
+lein uberjar
+cd target
+mkdir "$DIR"
+cp $(ls bltool-*-standalone.jar | tail -n1) "$DIR"
+cp ../steam2backloggery.bat "$DIR"
+zip -r "bltool-$VERSION.zip" "$DIR"
+rm -rf "$DIR"
---
+++ steam2backloggery.bat
@@ -0,0 +1,8 @@
+set STEAM_NAME=steam username
+set BL_NAME=backloggery username
+set BL_PASS=backloggery password
+set EDITOR=notepad
+
+java -jar bltool.jar --steam-name "%STEAM_NAME%" --bl-name "%BL_NAME%" --bl-pass "%BL_PASS" --filter --from steam --to text --output games.txt
+%EDITOR% games.txt
+java -jar bltool.jar --bl-name "%BL_NAME%" --bl-pass "%BL_PASS" --from text --to edn --input games.txt --output games.edn
