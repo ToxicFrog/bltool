bd938836804c6454929caca76535256934edc2d3
Ben Kelly <bk@ancilla.ca>
2013-12-15 01:49:45 -0500

    Update release script to name the jar "bltool.jar"

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ DIR="bltool-$VERSION"
 lein uberjar
 cd target
 mkdir "$DIR"
-cp $(ls bltool-*-standalone.jar | tail -n1) "$DIR"
+cp $(ls bltool-*-standalone.jar | tail -n1) "$DIR/bltool.jar"
 cp ../steam2backloggery.bat "$DIR"
 zip -r "bltool-$VERSION.zip" "$DIR"
 rm -rf "$DIR"
