07209bf992f8373353381320a65182e3eadeb66a
Ben Kelly <bk@ancilla.ca>
2013-12-27 11:30:46 -0500

    steam2backloggery hasn't been working this entire time because for some reason it was writing to EDN

--- steam2backloggery.bat
+++ steam2backloggery.bat
@@ -5,4 +5,4 @@ set EDITOR=notepad
 
 java -jar bltool.jar --steam-name "%STEAM_NAME%" --bl-name "%BL_NAME%" --bl-pass "%BL_PASS%" --filter --from steam --to text --output games.txt
 %EDITOR% games.txt
-java -jar bltool.jar --bl-name "%BL_NAME%" --bl-pass "%BL_PASS%" --from text --to edn --input games.txt --output games.edn
+java -jar bltool.jar --bl-name "%BL_NAME%" --bl-pass "%BL_PASS%" --from text --to backloggery --input games.txt
