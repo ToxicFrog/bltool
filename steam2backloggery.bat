f906fbb0cfa616a340b648a5f605790c0faba032
Ben Kelly <bk@ancilla.ca>
2014-01-13 19:17:39 -0500

    Make steam2backloggery.bat a bit more self-documenting

--- steam2backloggery.bat
+++ steam2backloggery.bat
@@ -1,7 +1,15 @@
-set STEAM_NAME=steam username
+REM =====================
+REM USER SETTINGS GO HERE
+REM =====================
+
+set STEAM_NAME=steamcommunity ID
 set BL_NAME=backloggery username
 set BL_PASS=backloggery password
 set EDITOR=notepad
+
+REM =========================================
+REM NO USER SERVICEABLE PARTS BELOW THIS LINE
+REM =========================================
 
 java -jar bltool.jar --steam-name "%STEAM_NAME%" --bl-name "%BL_NAME%" --bl-pass "%BL_PASS%" --filter --from steam --to text --output games.txt
 %EDITOR% games.txt
