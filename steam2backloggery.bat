8e711716d916909147d24baf73763bc67a1c4886
Ben Kelly <bk@ancilla.ca>
2014-01-31 23:05:49 -0500

    Add some common Java paths to %PATH%, and change the .bat file to use CRLF newlines :(

--- steam2backloggery.bat
+++ steam2backloggery.bat
@@ -11,6 +11,12 @@ REM =========================================
 REM NO USER SERVICEABLE PARTS BELOW THIS LINE
 REM =========================================
 
+REM Java on windows is a complete nightmare, and doesn't always properly get
+REM added to %PATH%. So we add a bunch of plausible directories here.
+
+SET PATH=%PATH%;"C:\Program Files\Java\jre7\bin;C:\Program Files\Java\jre6\bin"
+SET PATH=%PATH%;"C:\Program Files (x86)\Java\jre7\bin;C:\Program Files (x86)\Java\jre6\bin"
+
 java -jar bltool.jar --steam-name "%STEAM_NAME%" --bl-name "%BL_NAME%" --bl-pass "%BL_PASS%" --filter --from steam --to text --output games.txt
 %EDITOR% games.txt
 java -jar bltool.jar --bl-name "%BL_NAME%" --bl-pass "%BL_PASS%" --from text --to backloggery --input games.txt
