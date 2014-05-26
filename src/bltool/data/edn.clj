db9761d6ae1a13e5ee6ee73130b4a346099ce49b
Ben Kelly <bk@ancilla.ca>
2014-05-25 21:15:47 -0400

    Use pprint for edn output

--- src/bltool/data/edn.clj
+++ src/bltool/data/edn.clj
@@ -1,10 +1,12 @@
 (ns bltool.data.edn
   (:require [bltool.data.default :refer :all])
   (:require [bltool.flags :refer :all])
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn])
+  (:require [clojure.pprint :as pprint]))
 
 (defmethod read-games "edn" [_ source]
   (edn/read (java.io.PushbackReader. source)))
 
 (defmethod write-games "edn" [_ games sink]
-  (.write sink (str (vec games))))
+  (pprint/pprint (vec games) sink)
+  (.write sink "\n"))
